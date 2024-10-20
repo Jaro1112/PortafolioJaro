2fb18ceb60bf4eac82fa01ea1b4f4fd5f09df7a7
julian rodriguez <julian.rodriguezoc@campusucc.edu.co>
2024-10-19 19:48:23 -0500

    responsive para celulares

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
+    // Establecer el estilo inicial del header
+    header.style.backgroundColor = '#161b1a';
+    header.style.padding = '10px'; // Asegúrate de que el padding sea 10px al cargar
   
     window.addEventListener('scroll', function() {
       if (window.scrollY > 50) {
         // Reducir el tamaño de la navbar y hacerla más opaca
         header.style.backgroundColor = '#161b1a';
-        header.style.padding = '10px'; // Asegúrate de que este valor coincida con el padding por defecto
+        header.style.padding = '10px';
       } else {
         // Restaurar el tamaño original
         header.style.backgroundColor = '#161b1a';
-        header.style.padding = '10px'; // Asegúrate de que este valor coincida con el padding por defecto
+        header.style.padding = '10px';
       }
     });
   });
@@ -30,29 +33,30 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
-  
-  let currentImageIndex = 0;
 
   function changeImage(direction) {
-    const images = document.getElementsByClassName("carousel-image");
-    images[currentImageIndex].classList.remove("active");
-    currentImageIndex = (currentImageIndex + direction + images.length) % images.length;
-    images[currentImageIndex].classList.add("active");
-    updateIndicators();
+    const images = document.querySelectorAll('.carousel-image');
+    let currentIndex = Array.from(images).findIndex(image => image.classList.contains('active'));
+    images[currentIndex].classList.remove('active');
+
+    currentIndex = (currentIndex + direction + images.length) % images.length;
+    images[currentIndex].classList.add('active');
+
+    // Actualiza los indicadores
+    updateIndicators(currentIndex);
   }
 
   function setCurrentImage(index) {
-    const images = document.getElementsByClassName("carousel-image");
-    images[currentImageIndex].classList.remove("active");
-    currentImageIndex = index;
-    images[currentImageIndex].classList.add("active");
-    updateIndicators();
+    const images = document.querySelectorAll('.carousel-image');
+    images.forEach(image => image.classList.remove('active'));
+    images[index].classList.add('active');
+
+    // Actualiza los indicadores
+    updateIndicators(index);
   }
 
-  function updateIndicators() {
-    const indicators = document.getElementsByClassName("indicator");
-    for (let i = 0; i < indicators.length; i++) {
-      indicators[i].classList.remove("active");
-    }
-    indicators[currentImageIndex].classList.add("active");
+  function updateIndicators(index) {
+    const indicators = document.querySelectorAll('.indicator');
+    indicators.forEach(indicator => indicator.classList.remove('active'));
+    indicators[index].classList.add('active');
   }
